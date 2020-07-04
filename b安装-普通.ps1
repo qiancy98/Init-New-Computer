0ae84abd34c786a0eb0dfd2a49de87c11bd42cd9
QianCY <q-young@qq.com>
2020-07-04 08:40:58 +0800

    更新MiKTeX安装代码

--- b安装-普通.ps1
+++ b安装-普通.ps1
@@ -5,7 +5,9 @@
 # Set-ExecutionPolicy RemoteSigned -Scope Process
 
 # MikTeX配置
-winget install -e --id ChristianSchenk.MiKTeX
+#winget install -e --id ChristianSchenk.MiKTeX
+& "D:\Program Files\~安装包\01 LaTeX编译器\basic-miktex-2.9.7219-x64.exe" --auto-install=yes --unattended
+Pause
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 mpm --repository=http://mirrors.sjtug.sjtu.edu.cn/ctan/systems/win32/miktex/tm/packages/
 Write-Output "MiKTeX: 升级软件包..."
