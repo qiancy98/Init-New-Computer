4c1629e9063a0471ccb08c9de83a8fa58d328184
QianCY <q-young@qq.com>
2020-07-10 04:52:31 +0800

    c更新配置.ps1修改

--- c更新配置.ps1
+++ c更新配置.ps1
@@ -1,2 +1,4 @@
-Copy-Item "$env:USERPROFILE\.gitconfig"         ".\其他脚本\安装\Git\.gitconfig"    
-Copy-Item "$env:APPDATA\Code\User\syncing.json" ".\其他脚本\安装\Code\syncing.json"
+Copy-Item "$env:USERPROFILE\.gitconfig"                  ".\其他脚本\安装\Git\.gitconfig"    
+Copy-Item "$env:APPDATA\FreeFileSync\GlobalSettings.xml" ".\其他脚本\安装\FreeFileSync\GlobalSettings.xml"    
+Copy-Item "$env:APPDATA\FreeFileSync\LastRun.ffs_gui"    ".\其他脚本\安装\FreeFileSync\LastRun.ffs_gui"       
+# Copy-Item "$env:APPDATA\Code\User\syncing.json" ".\其他脚本\安装\Code\syncing.json" #因为已经设成只读
