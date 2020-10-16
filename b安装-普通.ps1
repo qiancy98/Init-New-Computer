cd6121eb09bd271138c10d3bbf15a184c38b5252
QianCY <q-young@qq.com>
2020-10-16 20:17:02 +0800

    更新MiKTeX安装代码

--- b安装-普通.ps1
+++ b安装-普通.ps1
@@ -9,7 +9,7 @@
 & "D:\Program Files\~安装包\01 LaTeX编译器\basic-miktex-2.9.7219-x64.exe" --auto-install=yes --unattended
 Pause
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-mpm --repository=http://mirrors.sjtug.sjtu.edu.cn/ctan/systems/win32/miktex/tm/packages/
+mpm --set-repository=https://mirrors.sjtug.sjtu.edu.cn/ctan/systems/win32/miktex/tm/packages/
 Write-Output "MiKTeX: 升级软件包..."
 mpm --update
 Write-Output "MiKTeX: 安装剩余软件包..."
