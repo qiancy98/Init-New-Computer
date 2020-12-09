2c4835d950ad72294292befaba283f7c6804c2c8
QianCY <q-young@qq.com>
2020-12-10 04:43:21 +0800

    Ruby

---
+++ Ubuntu/scripts/Ruby/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# 安装ruby
+sudo apt-get install ruby-full build-essential -y # zlib1g-dev
+# 添加根目录
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+# 安装jekyll
+gem install jekyll bundler
+# 用来使yekyll支持UTF+8编码.
+s=$(ruby -v)
+sudo cp --verbose filehandler.rb /usr/lib/ruby/${s:5:5}/webrick/httpservlet/filehandler.rb
