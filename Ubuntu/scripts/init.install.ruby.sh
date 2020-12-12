bf68227b418d3e281b7fbb813ce538480627bb59
QianCY <q-young@qq.com>
2020-12-13 00:10:28 +0800

    ruby安装时 运行会打印命令

--- Ubuntu/scripts/init.install.ruby.sh
+++ Ubuntu/scripts/init.install.ruby.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/bash
+# 运行命令前打印
+set -x
+
 # 安装ruby
 sudo apt-get install ruby-full build-essential -y # zlib1g-dev
+
 # 添加根目录
 echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
+
 # 安装jekyll
 gem install jekyll bundler
+
 # 用来使yekyll支持UTF+8编码.
 s=$(ruby -v)
 sudo cp --verbose init.install.ruby.filehandler.rb /usr/lib/ruby/${s:5:5}/webrick/httpservlet/filehandler.rb
