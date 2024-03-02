3ea7c0b7aafc4df17e0ed9c33b6b57360e0d8716
QianCY <q-young@qq.com>
2024-03-03 05:32:52 +0800

    更新密码生成器，可以生成任意长的密码（默认15位）

--- 其他脚本/密码生成器.py
+++ 其他脚本/密码生成器.py
@@ -1,14 +1,28 @@
-import secrets
+# 如果输入整数，输出整数长的密码；否则，输出15位密码
+长度 = 15
+
+import sys
+
+if len(sys.argv) > 1:
+    try:
+        长度 = int(sys.argv[1])
+    except ValueError:
+        长度 = 15
+
+
+def sample(字符串集: str, n: int) -> str:
+    import secrets
+
+    return "".join(secrets.choice(字符串集) for _ in range(n))
+
+
 import string
 
-def sample(字符串集:str, n:int) -> str:
-	return [secrets.choice(字符串集) for i in range(n)]
-
-随机数 = ''.join(sample(string.ascii_letters + string.digits + string.punctuation,15))
+随机数 = sample(string.ascii_letters + string.digits + string.punctuation, 长度)
 print(随机数)
 
-随机数 = ''.join(sample(string.ascii_letters + string.digits,15))
+随机数 = sample(string.ascii_letters + string.digits, 长度)
 print(随机数)
 
-随机数 = ''.join(sample(string.digits,15))
+随机数 = sample(string.digits, 长度)
 print(随机数)
